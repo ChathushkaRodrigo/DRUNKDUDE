f9bab7dba297fd3ef8355a0fb2121c6f01cc8f67
Chathushka Rodrigo <72795289+ChathushkaRodrigo@users.noreply.github.com>
2021-03-13 00:20:03 +0530

    Added Main Files

---
+++ Assets/Scrpits/LectureFollowUp/Lectures/src/l1.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class l1{
+     public static void main(String[] args) {
+        
+        // System.out.println("Chathushka");
+        // System.out.println("Seniya");
+        // System.out.println("Rodrigo");
+
+//using scanner class
+        
+
+
+        // float len , wid, perimeter;
+
+        Scanner ob = new Scanner(System.in);
+      
+        // System.out.println("Enter Length: ");
+        // len = ob.nextFloat();
+
+        // System.out.println("Enter Width: ");
+        // wid = ob.nextFloat();
+
+        // perimeter = len*2 + wid*2;
+
+        // System.out.println("Permeter of the recatangle is : "+ perimeter);
+        float num1,num2,num3;
+
+        System.out.println("Enter value for the first number: ");
+        num1 =ob.nextFloat();
+
+        System.out.println("Enter value for the second number: ");
+        num2 =ob.nextFloat();
+
+        System.out.println("Enter value for the third number: ");
+        num3 =ob.nextFloat();
+
+
+        if(num3 < num1> num2){
+            System.out.println(num1);
+        }
+        
+
+
+
+
+
+
+
+
+
+        
+        } 
+            
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Scrpits/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rbody;
+    public float speed = 10f;
+    public float count;
+    public Text countText;  
+    public Text winText;
+
+    void Start()
+    {
+        count = 0;
+        countText.text = "";
+        winText.text = "";
+        rbody= GetComponent<Rigidbody2D>();
+        
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+
+        rbody.AddForce(new Vector2(horizontalInput, verticalInput) *speed);
+    }
+
+        void OnTriggerEnter2D( Collider2D target)
+        {
+          if (target.gameObject.CompareTag("PickUp"))
+          {
+            Destroy(target.gameObject);
+            Count();
+          }
+
+        }
+        void Count(){
+            count +=10;
+            countText.text = "SCORE: "+ count.ToString(); 
+
+            if(count >= 50)
+            {
+                winText.text = "YOU WIN!";
+                
+            }
+             
+        }
+}//class
+ 
